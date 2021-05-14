2daf4d49254be6961145dd5804975ee1db5b8881
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-05-14 15:39:34 -0400

    Sync

--- ps-flux/src/Components/App.js
+++ ps-flux/src/Components/App.js
@@ -4,11 +4,15 @@ import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./Common/Header";
+import Courses from "./CoursesPage";
+import CoursesPage from "./CoursesPage";
 
 function App() {
   function getPage() {
     const route = window.location.pathname;
     if (route === "/about") return <AboutPage />;
+    if (route === "/courses") return <CoursesPage />;
+
     return <HomePage />;
   }
 
---
+++ ps-flux/src/Components/CoursesPage.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { getCourses } from "../api/courseApi";
+
+class CoursesPage extends React.Component {
+  state = {
+    courses: [],
+  };
+
+  //this is the proper method for making api calls
+  componentDidMount() {
+    getCourses().then((courses) => this.setState({ courses: courses }));
+  }
+
+  render() {
+    return (
+      <>
+        <h2>Courses</h2>;
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author Id</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.courses.map((course) => {
+              return (
+                <tr>
+                  <td>{course.title}</td>
+                  <td>{course.authorId}</td>
+                  <td>{course.category}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
+
+export default CoursesPage;
