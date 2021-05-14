ed46e27170db4fd23dd1921d836730201527a484
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-05-14 15:11:11 -0400

    Sync

---
+++ ps-flux/src/Components/App.js
@@ -0,0 +1,13 @@
+//decide which page to render
+
+import React from "react";
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+
+function App() {
+  const route = window.location.pathname;
+  if (route === "/about") return <AboutPage />;
+  return <HomePage />;
+}
+
+export default App;
--- ps-flux/src/index.js
+++ ps-flux/src/index.js
@@ -1,11 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import React from 'react';
+import React from "react";
 
 //using the {} syntax is basically instantiating a constructor for the render() from the react-dom library
-import {render} from 'react-dom';
-import HomePage from './Components/HomePage';
-import AboutPage from './Components/AboutPage';
+import { render } from "react-dom";
+import App from "./Components/App";
 
 //this "root" div in index.html is given to us by create-react-app, and is essentially the React counterpart of "@RenderBody".
-//the index.js file however, is our true "entry point", the div with the Id of "root" in index.html is just where it's getting plopped in. 
-render(<AboutPage/>, document.getElementById("root"));
+//the index.js file however, is our true "entry point", the div with the Id of "root" in index.html is just where it's getting plopped in.
+render(<App />, document.getElementById("root"));
