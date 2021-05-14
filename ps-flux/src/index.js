ae4b47922c3c3d00d0fbf73d1e1e8686bffc11eb
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-05-14 15:03:21 -0400

    Sync

--- ps-flux/src/index.js
+++ ps-flux/src/index.js
@@ -1,6 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
+
+//using the {} syntax is basically instantiating a constructor for the render() from the react-dom library
 import {render} from 'react-dom';
 import HomePage from './Components/HomePage';
+import AboutPage from './Components/AboutPage';
 
-render(<HomePage/>, document.getElementById("root"));
+//this "root" div in index.html is given to us by create-react-app, and is essentially the React counterpart of "@RenderBody".
+//the index.js file however, is our true "entry point", the div with the Id of "root" in index.html is just where it's getting plopped in. 
+render(<AboutPage/>, document.getElementById("root"));
