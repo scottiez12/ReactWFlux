4250261b5176840088b41880212d54bffe536f50
Scott Ziegler <47644964+scottiez12@users.noreply.github.com>
2021-05-14 15:16:53 -0400

    Sync

--- ps-flux/src/Components/App.js
+++ ps-flux/src/Components/App.js
@@ -3,11 +3,21 @@
 import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
+import Header from "./Common/Header";
 
 function App() {
-  const route = window.location.pathname;
-  if (route === "/about") return <AboutPage />;
-  return <HomePage />;
+  function getPage() {
+    const route = window.location.pathname;
+    if (route === "/about") return <AboutPage />;
+    return <HomePage />;
+  }
+
+  return (
+    <div class="container-fluid">
+      <Header />
+      {getPage()}
+    </div>
+  );
 }
 
 export default App;
